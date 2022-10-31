a79f20c93b2f202ac980333ddff3ecd9cf15d37d
boonruang <scubatoy@gmail.com>
2022-10-31 21:28:13 +0700

    19. Allowing to display the HTTP Client messages in any location

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,12 +42,12 @@
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
-{{-- 
+
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
-                            @endif --}}
+                            @endif
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -71,6 +71,28 @@
                 </div>
             </div>
         </nav>
+
+        @if(isset($errors) && $errors->any())
+        <div class="alert alert-danger">
+            <button class="close" type="button" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+
+        @if(session()->has('success'))
+        <div class="alert alert-success">
+            <button class="close" type="button" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <ul>
+                @foreach ($session->get('success') as $message)
+                    <li>{{$message}}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
 
         <main class="py-4">
             @yield('content')
