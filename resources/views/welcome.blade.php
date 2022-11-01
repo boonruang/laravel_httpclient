0e9d2a21772c92fa3a4af865645de8e2aed95ec5
boonruang <scubatoy@gmail.com>
2022-11-01 21:36:40 +0700

    31. Adding the list of categories to the main page of the HTTP Client

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,22 +3,47 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-        <div class="col">
-            <h2 class="display-2">Products</h2>
-        </div>
-    </div>
-    <div class="row">
-        @foreach ($products as $product)
-            <div class="col-4">
-                <div class="card">
-                    <img src="{{$product->picture}}" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">{{$product->title}}</h5>
-                        <p class="card-text">{{$product->details}}</p>
-                    </div>
+        <!-- Left Column-->
+        <div class="col-2">
+            <div class="row">
+                <div class="col">
+                    <h2>Categories</h2>
                 </div>
             </div>
-        @endforeach
+            <div class="row">
+                <div class="col">
+                    <ul class="list-group">
+                        @foreach ($categories as $category)
+                            <a href="#" class="list-group-item">{{$category->title}}</a>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- End Left Column-->
+
+        <!-- Right Column-->
+        <div class="col">
+            <div class="row">
+                <div class="col">
+                    <h2 class="display-2">Products</h2>
+                </div>
+            </div>
+            <div class="row">
+                @foreach ($products as $product)
+                    <div class="col-4">
+                        <div class="card">
+                            <img src="{{$product->picture}}" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">{{$product->title}}</h5>
+                                <p class="card-text">{{$product->details}}</p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>            
+        </div>
+        <!-- End Right Column-->
     </div>
 </div>
 @endsection
