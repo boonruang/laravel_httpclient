b9c18a44f296bd02038a2a5c2ba6ef815c9dc24a
boonruang <scubatoy@gmail.com>
2022-11-01 17:18:05 +0700

    24. Implementing the method to authorize requests

--- config/services.php
+++ config/services.php
@@ -21,6 +21,14 @@ return [
         'scheme' => 'https',
     ],
 
+    'market' => [
+        'base_uri' => env('MARKET_BASE_URI'),
+        'client_id' => env('MARKET_CLIENT_ID'),
+        'client_secret' => env('MARKET_CLIENT_SECRET'),
+        'password_client_id' => env('MARKET_PASSWORD_CLIENT_ID'),
+        'password_client_secret' => env('MARKET_PASSWORD_CLIENT_SECRET'),
+    ],
+
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
