fd2a33c68e0c417f5929f2c15826fc413dd16911
boonruang <scubatoy@gmail.com>
2022-11-02 12:07:29 +0700

    36. Adding a new service to obtain access tokens from the HTTP Client

---
+++ app/Services/MarketAuthenticationService.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Services;
+
+use App\Traits\ConsumesExternalServices;
+use App\Traits\InteractsWithMarketResponses;
+
+class MarketAuthenticationService {
+    use ConsumesExternalServices, InteractsWithMarketResponses;
+
+    /**
+     * The URL to send the requests
+     * @var string
+     */ 
+
+    protected $baseUri;
+
+    /**
+     * The client id to identify the client in the API
+     * @var string
+     */ 
+
+    protected $clientId;
+    
+    /**
+     * The client secret to identify the client in the API
+     * @var string
+     */ 
+
+    protected $clientSecret;
+    
+    /**
+     * The client id to identify the password client in the API
+     * @var string
+     */ 
+
+    protected $passwordClientId;
+    
+    
+    /**
+     * TThe client secret to identify the password client in the API
+     * @var string
+     */ 
+
+    protected $passwordClientSecret;    
+
+    public function __construct(){
+        $this->baseUri = config('services.market.base_uri');
+        $this->clientId = config('services.market.client_id');
+        $this->clientSecret = config('services.market.client_secret');
+        $this->passwordClientId = config('services.market.password_client_id');
+        $this->passwordClientSecret = config('services.market.password_client_secret');
+    }
+
+}
+
